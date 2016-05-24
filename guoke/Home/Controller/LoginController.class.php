28ab6a55f3e7dfec86fa8edce52acb8a27583c76
lgg <976513843@qq.com>
2016-05-24 12:53:04 +0800

    liguojiang

---
+++ guoke/Home/Controller/LoginController.class.php
@@ -0,0 +1,60 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+//用户登录控制器.
+class LoginController extends Controller {
+	//用户登录首页
+    public function index(){
+        // var_dump($_COOKIES);
+        // var_dump($_SESSION);
+    	$this->display();
+    }
+
+    public function Emaildo(){
+        $m=M('UserVerify');
+        $where="`User_Email`='".$_GET['email']."'";
+        $res=$m->where($where)->find();
+        // var_dump($res);
+        if(!$res){
+            $this->ajaxReturn(1);
+        }
+    }
+
+    public function yzm(){
+        $config = array(
+        'reset' => false // 验证成功后是否重置，这里才是有效的。
+        );
+        $verify = new \Think\Verify($config);
+            if($verify->check(I('post.code'))){
+                   $this->ajaxReturn(1);
+            }
+    }
+
+    public function logindo(){
+        // var_dump($_POST);
+        // die;
+        $m=M('userVerify');
+        $w="`User_Email`='".$_POST['email']."'AND `User_Pwd`='".$_POST['pass']."'";
+        $res=$m->where($w)->find();
+        // echo $m->_sql();
+        // var_dump($res);
+        //如果成功之后吧用户的UID返回给ajax
+        if($res!=null){
+            session_start();
+            $_SESSION['Home']['Uid']=$res['U_id'];
+            $_SESSION['Home']['Email']=$res['User_Email'];
+        if($_POST['checked']=='true'){
+            cookie('useremail',$res['User_Email'],array('expire'=>3600,'prefix'=>'HomeUser_'));
+            cookie('password',$res['User_Pwd'],array('expire'=>3600,'prefix'=>'HomeUser_'));
+        }
+            $this->ajaxReturn($res['U_id']);
+        }
+    }
+
+
+
+
+
+
+
+}
---
+++ guoke/Home/Controller/PublicController.class.php
@@ -0,0 +1,17 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class PublicController extends Controller {
+
+	public function CreateVcode(){
+		$config =array(
+		'length'=>2,
+		'fontSize'    =>    30,
+		'useNoise'    =>    false,
+		);
+		$Verify = new \Think\Verify($config);
+		$Verify->entry();
+	}
+
+
+}
