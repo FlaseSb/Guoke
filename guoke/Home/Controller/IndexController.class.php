b711c0cd6a762df23ff9c613ed187f8824c3e40c
sbc5101 <541891006@qq.com>
2016-05-25 11:24:10 +0800

    shenbinchao

--- guoke/Home/Controller/IndexController.class.php
+++ guoke/Home/Controller/IndexController.class.php
@@ -3,13 +3,9 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends PublicController {
     public function index(){
-
-<<<<<<< HEAD
+    	
     	var_dump($_COOKIE);
-=======
-    	// var_dump($_COOKIE);
 
->>>>>>> 588a3fdb7fbf905acca9ee8c6432c710a590faa5
     	$this->display();
 
     }
