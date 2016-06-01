d3470244ba7d790f05563fa55b9a999b287ec83e
sbc5101 <541891006@qq.com>
2016-06-01 14:38:34 +0800

    第一次

---
+++ guoke/guoke/Admin/Controller/QueexamineController.class.php
@@ -0,0 +1,119 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+//后台的问题审核管理控制器.
+class QueexamineController extends CommonController {
+	//问题审核列表显示板块
+    public function index(){
+        // 接收数据
+        $name=$_GET['name'];
+        $state=$_GET['num'];
+        $sub=M('qac_subject');
+        // 获取显示数量
+        $num=!empty($_GET['page'])?$_GET['page']:5;
+        //获取关键字
+        if(!empty($_GET['keyword'])){
+            $where=" and sub_name like '%".$_GET['keyword']."%'";
+        }else{
+            $where="";
+        }
+        $order="'id asc'";
+        if(!empty($name) && $state==null){
+            $order=$name.' desc';
+            $state=1;
+        }elseif($state==1){
+            $order=$name.' asc';
+            $state=null;
+        }
+        // 查询满足条件总记录数
+        $count=$sub->where("is_examine='0'".$where)->count();
+        //实例化分页类
+        $page=new \Think\Page($count,$num);
+        // 获取limit参数
+        $limit=$page->firstRow.','.$page->listRows;
+
+        $res=$sub->join('left join user_verify on qac_subject.user_id = user_verify.U_id')->where("is_examine='0'".$where)->order($order)->limit($limit)->select();
+        foreach ($res as $k => $v) {
+            $res[$k]['sub_name']=str_replace(keyword(),highlight(),$v['sub_name']);
+            $res[$k]['merge']=$sub->where('is_examine="1" and sub_name="'.$v['sub_name'].'"')->select();
+        }
+        // var_dump($res[3]['merge']);die;
+        // 分页输出
+        $pages=$page->show();
+
+        // var_dump($page);die;
+        $this->assign('state',$state);
+        $this->assign('pages',$pages);
+        $this->assign('num',$num);
+        $this->assign('res',$res);
+    	$this->display();
+    }
+
+    // ajax通过模块
+    public function adopt(){
+        // 设置通过
+        $is_examine['is_examine']=1;
+
+        $id=$_GET['id'];
+        $sub=M('qac_subject');
+        //执行修改
+        $res=$sub->where('id='.$id)->save($is_examine);
+        header('location:'.$_SERVER['HTTP_REFERER']);
+
+    }
+
+    // ajax详细描述模块
+    public function ajaxdetailed(){
+        // 获取id
+        $id=$_GET['id'];
+        $sub=M('qac_subject');
+        $res=$sub->where('id='.$id)->select();
+        // var_dump($res);
+        //将sub_msg转换为实体;
+        $sub_msg=htmlspecialchars_decode($res['0']['sub_msg']);
+
+        echo $sub_msg;
+        // $this->display();
+
+    }
+
+    // 问题删除模块
+    public function ajaxdel(){
+        // 接收id
+        $id=$_GET['id'];
+        
+        $sub=M('qac_subject');
+
+        // 执行删除
+        $res=$sub->where('id='.$id)->delete();
+
+        // 判断是否删除
+        if($res){
+            echo 1;
+        }else{
+            echo 0;
+        }
+    }
+
+    // 合并相同信息
+    public function merge(){
+        // 获取pid
+        $pid=$_POST['pid'];
+        // 获取当前id
+        $id=$_POST['id'];
+        // 获取当前用户id
+        $uid=$_POST['uid'];
+        $data['sub_id']=$pid;
+
+        $sub=M('qac_subject');
+        $ans=M('qac_answer');
+        $ta=M('qac_tag_answer');
+        $fol=M('qac_follow');
+
+        $sub->where('id='.$id)->delete();
+        $ans->where('sub_id='.$id)->save($data);
+        $ta->where('sub_id='.$id)->save($data);
+        $fol->where('sub_id='.$id)->save($data);
+    }
+
+}
